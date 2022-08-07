32828306e346a86cace8e6e4583f08f87d6bfd16
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2022-08-07 15:34:57 +0300

    ref: Rename 5.0.3 to 5.0.5
    There is no difference in typeids but I use 5.0.5 as reference version
    for this asset in cpp-sc2.
    
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- test/cpp/generate.spec.js
+++ test/cpp/generate.spec.js
@@ -7,7 +7,7 @@ const stableIDs4083 = require('../../assets/4.8.3-stableid.json')
 const stableIDs4104 = require('../../assets/4.10.4-stableid.json')
 const stableIDs4114 = require('../../assets/4.11.4-stableid.json')
 const stableIDs4120 = require('../../assets/4.12.0-stableid.json')
-const stableIDs5003 = require('../../assets/5.0.3-stableid.json')
+const stableIDs5005 = require('../../assets/5.0.5-stableid.json')
 const stableIDs5010 = require('../../assets/5.0.10-stableid.json')
 
 test('Generates c++ code for Sc2 v4.8.3', () => {
@@ -34,8 +34,8 @@ test('Generates c++ code for Sc2 v4.12.0', () => {
   expect(fs.generatedCPP).toMatchSnapshot()
 })
 
-test('Generates c++ code for Sc2 v5.0.3', () => {
-  generate(stableIDs5003)
+test('Generates c++ code for Sc2 v5.0.5', () => {
+  generate(stableIDs5005)
   expect(fs.generatedHeader).toMatchSnapshot()
   expect(fs.generatedCPP).toMatchSnapshot()
 })
